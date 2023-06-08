752b876e61ddc7b2d8e5ee40e870456825aac02e
Kavishcan <108811385+Kavishcan@users.noreply.github.com>
2023-06-08 16:02:46 +0530

    make intersection obs work

--- script.js
+++ script.js
@@ -13,9 +13,9 @@ console.log(h1);
 
 ///////////////////////////////////////////////////////////
 // Set current year
-const yearEl = document.querySelector(".year");
-const currentYear = new Date().getFullYear();
-yearEl.textContent = currentYear;
+// const yearEl = document.querySelector(".year");
+// const currentYear = new Date().getFullYear();
+// yearEl.textContent = currentYear;
 
 ///////////////////////////////////////////////////////////
 // Make mobile navigation work
@@ -23,9 +23,9 @@ yearEl.textContent = currentYear;
 const btnNavEl = document.querySelector(".btn-mobile-nav");
 const headerEl = document.querySelector(".header");
 
-btnNavEl.addEventListener("click", function () {
-  headerEl.classList.toggle("nav-open");
-});
+// btnNavEl.addEventListener("click", function () {
+//   headerEl.classList.toggle("nav-open");
+// });
 
 ///////////////////////////////////////////////////////////
 // Smooth scrolling animation
